225e5139232ac4c3b3b0da0ad49e9fa47bbadfb6
Alberto Paiva <albertolpaiva@gmail.com>
2023-06-21 16:53:22 -0300

    Added Astro web framework

---
+++ src/custom-attributes.d.ts
@@ -0,0 +1,9 @@
+// Copyright 2023 Alberto Paiva.
+// SPDX-License-Identifier: MIT
+
+declare namespace astroHTML.JSX {
+  interface HTMLAttributes {
+    'data-count'?: number
+    'data-label'?: string
+  }
+}
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="astro/client" />
+
+declare global {
+  var myString: string
+  function myFunction(): boolean
+}
+
+// https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript
+interface ImportMetaEnv {
+  readonly SITE: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+export {}
