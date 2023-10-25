982d9eed3236d27e252aa16347836601edf88f67
Alberto Paiva <albertolpaiva@gmail.com>
2023-10-24 22:04:09 -0300

    Delete src directory

--- src/custom-attributes.d.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 Alberto Paiva.
-// SPDX-License-Identifier: MIT
-
-declare namespace astroHTML.JSX {
-  interface HTMLAttributes {
-    'data-count'?: number
-    'data-label'?: string
-  }
-}
